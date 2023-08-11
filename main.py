6b191dee8d3e2be8c394efef8642afc2ad97027a
yakov <k169300@yandex.ru>
2023-08-11 15:45:55 +0300

    Добавлено подключение к PostgreSQL

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import tkinter as tk
+import pandas as pd
+import psycopg2 as psql
+import psycopg2
+from psycopg2 import Error
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+
+
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -12,5 +22,28 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+
+
+try:
+    # Подключение к существующей базе данных
+    connection = psycopg2.connect(user="postgres",
+                                  # пароль, который указали при установке PostgreSQL
+                                  password="postgres",
+                                  host="127.0.0.1",
+                                  port="5432")
+    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    # Курсор для выполнения операций с базой данных
+    cursor = connection.cursor()
+    sql_create_database = 'drop database postgres_db'
+    cursor.execute(sql_create_database)
+except (Exception, Error) as error:
+    print("Ошибка при работе с PostgreSQL", error)
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Соединение с PostgreSQL закрыто")
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
